445c42ace6cbb44d534d098d664783519c0bb136
harry238 <hrdkrathod3@gmail.com>
2017-09-11 15:15:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,4 @@
-//counter Code
-var button = document.getElementById('counter');
-
-button.onclick = function (){
-    
-    //Create a request object
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            //Take Some action
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-            }
-        }
-        //Not done yet
-    };
-    
-    //Make the request
-    request.open('GET','http://hardikrathod62.imad.hasura-app.io/counter',true);
-    request.send(null);
-    
-};
-
-//Submit name
+//Submit username/password to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //MAke a request to the server and send the name 
@@ -38,23 +11,25 @@ submit.onclick = function(){
             //Take Some action
             if(request.status === 200){
                 //capture the list of name and render it as a list
-    var names = request.responseText;
-    names = JSON.parse(names);
-    var list = '';
-    for(var i=0; i< names.length; i++){
-        list +='<li>' + names[i] + '</li>';
+                console.log('user is logged in');
+                alert('Logged in Successfully');
+            } else if (request.status === 403){
+                alert('Username/Password is incorrect');
+            } else if (request.status === 500){
+                alert('Something went wrong on the server');
+            }
     }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;            }
-        }
-        //Not done yet
+            //Not done yet
     };
     
     //Make the request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET','http://hardikrathod62.imad.hasura-app.io/submit-name?name=' + name,true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(username);
+    request.open('POST','http://hardikrathod62.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
     
     
 };
