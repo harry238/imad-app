dc07b1ff7b6ba3e96c7f06d409784f9fc8b37852
harry238 <hrdkrathod3@gmail.com>
2017-09-11 20:32:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -223,8 +223,8 @@ app.get('/articles/:articleName', function(req,res){
             if(result.rows.length === 0){
                 res.status(404).send('Article not found');
             } else {
-                var articleData = result.rows[0];
-                res.send(createTemplate(articleData));
+                var articlesData = result.rows[0];
+                res.send(createTemplate(articlesData));
             }
         }
     });
